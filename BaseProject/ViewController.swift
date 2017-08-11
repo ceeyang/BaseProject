c3ede3914baa0c58cacc636ac210e346baa6184b
yangxichuan <yangxichuan@zhixueyun.com>
2017-08-11 11:01:28 +0800

    init base project with carthage

---
+++ BaseProject/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BaseProject
+//
+//  Created by 杨西川 on 11/08/2017.
+//  Copyright © 2017 杨西川. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import EZSwiftExtensions
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.random()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
